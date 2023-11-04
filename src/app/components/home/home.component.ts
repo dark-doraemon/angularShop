48f835ae0ead6bc76a5fac6e06dc79c609f969d0
PhamTrieu0703 <thanhtrieupham10@gmail.com>
2023-11-04 23:46:19 +0700

    slider2

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { SlideInterface } from '../imageSlider/types/slide.interface';
 
 @Component({
     selector: 'app-home',
@@ -8,6 +9,12 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent {
     constructor(private router: Router) { }
-    
+    slides: SlideInterface[] = [
+        { url: 'https://th.bing.com/th/id/R.48ae7063896075ef412c1ecfbc0350c5?rik=es8RJROOMry9JQ&pid=ImgRaw&r=0', title: 'beach' },
+        { url: 'https://hinhanhdep.net/wp-content/uploads/2015/12/anh-cho-va-meo-29.jpg', title: 'boat' },
+        { url: 'https://hinhanhdep.net/wp-content/uploads/2015/12/anh-cho-va-meo-36.jpg', title: 'forest' },
+        { url: 'https://hinhanhdep.net/wp-content/uploads/2015/12/anh-cho-va-meo-28.jpg', title: 'city' },
+        { url: 'https://img.meta.com.vn/Data/image/2021/09/21/anh-meo-con-35.jpg', title: 'italy' },
+      ];
 
 }
